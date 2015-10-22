227bb9c065fc349eacf58b496cb94e8005fc2c9b
Jonathan Frawley <jmf@9lines.org>
2015-10-22 22:02:01 +0100

    Add test command to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,31 @@
+import sys
+
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
 from setuptools import setup, find_packages
 setup(name = "redact-py",
         version = "0.1",
+	tests_require=['redis==2.10.3', 'pytest==2.8.2'],
+	cmdclass = {'test': PyTest},
         packages=['redact'],
         install_requires=['redis==2.10.3'])
